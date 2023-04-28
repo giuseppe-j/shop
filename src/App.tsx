0e118702246e08e691362652a7519c3827c85e21
giuse <giuseppecoluzzi87@gmail.com>
2023-04-28 12:44:05 +0200

    add tailwind and font

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='font-roboto'>Shop</h1>
+      <br />
+      <button className="primary">Primary</button>
+      <br />
+      <br />
+      <button className="secondary">Secondary</button>
+    </div>
+  )
+}
+
+export default App
